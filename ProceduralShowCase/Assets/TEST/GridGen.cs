3885bf29fb7186f24a8a7897b3315eaad7360253
Anneli Nilsson <blueprint.ace@gmail.com>
2019-12-30 16:03:05 +0100

    Rough island generation

---
+++ ProceduralShowCase/Assets/TEST/GridGen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GridGen : MonoBehaviour
+{
+
+    float[,] map;
+
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20.0f;
+
+    public float treeDensity = 0.5f;
+
+    public GameObject point;
+    public Terrain terrain;
+
+    void Start()
+    {
+        map = new float[width, height];
+
+        for ( int x = 0; x < width; x++ )
+        {
+            for ( int y = 0; y < height; y++ )
+            {
+                float xCoord = (float)x / width * scale;
+                float yCoord = (float)y / height * scale;
+
+                //Debug.Log("x: " + xCoord + " y: "+ yCoord + " Perlin noise " + Mathf.PerlinNoise(xCoord, yCoord));
+
+                float noiseValue = Mathf.PerlinNoise(xCoord, yCoord);
+
+                if ( noiseValue  < treeDensity )
+                {
+                    GameObject tree = Instantiate(point, new Vector3(x, terrain.terrainData.GetHeight(x, y), y), Quaternion.identity);
+                    tree.transform.localScale = new Vector3( 1, scale * noiseValue, 1 );
+                }
+                    
+            }
+        }
+    }
+
+
+}
---
+++ ProceduralShowCase/Assets/TEST/TerrainGen.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+
+public class TerrainGen : MonoBehaviour
+{
+    public int depth = 20;
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20.0f;
+
+    //public float xOffset = 100f;
+    //public float yOffset = 100f;
+
+    void Start ()
+    {
+        //xOffset = Random.Range(0f, 9999f);
+        //yOffset = Random.Range(0f, 9999f);
+
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+
+        for( int x = 0; x < width; x++)
+        {
+            for ( int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight ( int x, int y)
+    {
+        float xCoord = (float) x / width * scale; // + xOffset
+        float yCoord = (float) y / height * scale; // + yOffset
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
