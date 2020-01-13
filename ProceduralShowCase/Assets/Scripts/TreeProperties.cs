b7280b6c91ca607dd844c2a27123350994afc895
Anneli Nilsson <blueprint.ace@gmail.com>
2020-01-13 14:27:11 +0100

    Fixed water plane size, added tree life

---
+++ ProceduralShowCase/Assets/Scripts/TreeProperties.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class TreeProperties : MonoBehaviour
+{
+
+    public float lifeLength;
+    float lifeTime;
+
+    float life_timer_max = 0.2f;
+    int tick;
+
+  
+    void Awake()
+    {
+        lifeLength = Random.Range(0f, 100f);
+        tick = 0;
+    }
+
+    
+    void Update()
+    {
+        lifeTime += Time.deltaTime;
+        if(lifeTime >= life_timer_max)
+        {
+            lifeTime -= life_timer_max;
+            tick++;
+        }
+
+        if (tick > lifeLength)
+        {
+            Debug.Log(gameObject.name + " " + lifeLength);
+
+            Destroy(gameObject);
+        }
+            
+    }
+}
